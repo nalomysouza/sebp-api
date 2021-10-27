ba7de3760d3372c1bcc0c943d67d503a54591939
NALOMY DE SOUZA ALVES OLIVEIRA <nalomysouza@gmail.com>
2021-10-26 22:10:36 -0300

    Initial commit

---
+++ src/main/java/oas/nalomy/sebpapi/SebpApiApplication.java
@@ -0,0 +1,13 @@
+package oas.nalomy.sebpapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SebpApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SebpApiApplication.class, args);
+	}
+
+}

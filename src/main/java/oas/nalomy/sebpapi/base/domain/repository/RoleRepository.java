7af1cdbc4465ffd1629ec353b185b901bdf315c2
NALOMY DE SOUZA ALVES OLIVEIRA <nalomysouza@gmail.com>
2021-10-28 13:45:35 -0300

    corrige configuração

--- src/main/java/oas/nalomy/sebpapi/base/domain/repository/RoleRepository.java
+++ src/main/java/oas/nalomy/sebpapi/base/domain/repository/RoleRepository.java
@@ -1,12 +1,10 @@
 package oas.nalomy.sebpapi.base.domain.repository;
 
 import java.util.Optional;
-
+import oas.nalomy.sebpapi.base.domain.ERole;
+import oas.nalomy.sebpapi.base.domain.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import br.com.nalomy.models.security.ERole;
-import br.com.nalomy.models.security.Role;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {

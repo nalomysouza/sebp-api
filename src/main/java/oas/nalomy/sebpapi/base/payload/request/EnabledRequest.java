ecb3b9033698853a78f77ecb936b122f9fc1e335
NALOMY DE SOUZA ALVES OLIVEIRA <nalomysouza@gmail.com>
2021-10-28 23:11:44 -0300

    payload para request e response autenticação

---
+++ src/main/java/oas/nalomy/sebpapi/base/payload/request/EnabledRequest.java
@@ -0,0 +1,19 @@
+package oas.nalomy.sebpapi.base.payload.request;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EnabledRequest {
+	@NotNull
+	@Positive
+	private long id;
+
+	@NotNull
+	private boolean enabled;
+
+}
